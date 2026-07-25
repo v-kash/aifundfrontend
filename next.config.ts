c6a5b2c4fda1fcd1f4b2cd767942a3b3339b6c4f
v-kash <vikastrade50100@gmail.com>
2026-07-26 02:09:19 +0530

    nextconfig chnage

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone',
+//   async rewrites() {
+//     // Use 'backend' for Docker production, 'localhost' for local dev
+//     const apiUrl = process.env.NODE_ENV === 'production' 
+//       ? 'http://backend:8000/api/:path*' 
+//       : 'http://localhost:8000/api/:path*';
+      
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: apiUrl,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  async rewrites() {
-    // Use 'backend' for Docker production, 'localhost' for local dev
-    const apiUrl = process.env.NODE_ENV === 'production' 
-      ? 'http://backend:8000/api/:path*' 
-      : 'http://localhost:8000/api/:path*';
-      
-    return [
-      {
-        source: '/api/:path*',
-        destination: apiUrl,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
